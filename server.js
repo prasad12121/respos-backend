ceacb4706bb3f675d859dfe60eeb68c4d585679d
prasadpalase <prasadp.palase@gmail.com>
2025-12-03 13:12:19 +0530

    corrected front end url

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 // Allowed origins
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://resto-frontend-ten.vercel.app"
+  "https://https://res-pos-one.vercel.app"
 ];
 
 // -------------------------
